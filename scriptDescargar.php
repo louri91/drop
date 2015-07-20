080b1331babfde26621e3164da9a599b263e3316
Amanda Fernandez <louri1991@gmail.com>
2015-07-20 12:53:43 +0200

    De momento descarga png, jpg y pdf ;)

--- scriptDescargar.php
+++ scriptDescargar.php
@@ -1,28 +1,36 @@
-<!-- 
-
- Script PHP que introduce un nuevo archivo en el sistema.
-
--->
-
 <?php
 include_once ('scriptConexionBD.php');
 $conn = dbConnect();
 $id = $_GET['id'];
-
-$sql = "SELECT nombre, contenido, tipo, tamanio FROM archivos WHERE id='$id';";
-
+$sql = "SELECT * FROM archivos WHERE id='$id';";
 $result = mysqli_query($conn, $sql);
+$archivo = mysqli_fetch_array($result);
+$nombre = $archivo['nombre'];
+$contenido = $archivo['contenido'];
+$tipo = $archivo['tipo'];
+$tamanio = $archivo['tamanio'];
+echo '<title>';
+echo $nombre;
+echo '</title>';
 
-while ($archivo = mysqli_fetch_assoc($result)) {
-    $nombre = $archivo['nombre'];    
-    $contenido = $archivo['contenido'];
-    $tipo = $archivo['tipo'];
-    $tamanio = $tamanio['tamanio'];
+if($tipo=="image/png"){
+	echo '<img src="data:image/png;base64,'.base64_encode($contenido).'">';
+}
+if($tipo=="image/jpeg"){
+	echo '<img src="data:image/jpeg;base64,'.base64_encode($contenido).'">';
 }
 
-    header("Content-type: $tipo");
-    //header("Content-length: $tamanio"); 
-    //header("Content-Disposition: inline; filename=$nombre"); 
+header("Content-type:". $archivo['tipo']);
+
+if($tipo=="application/pdf"){
+	print $contenido;
+}
+
+//header("Content-type:". $archivo['tipo']);
+//header("Content-length:". $tamanio);
+//header("Cache-control: private");
+//header("Content-Disposition: inline; filename=$nombre");
 //header("Content-Disposition: attachment; filename='Practica1.pdf'");
-print $contenido;
+//echo $archivo['contenido'];
+//echo base64_decode($archivo['contenido']);
 ?>
